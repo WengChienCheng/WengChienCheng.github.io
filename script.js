cc84d391496ec61fa20cde5a3cb92a3a23c39696
WengChienCheng <69505061+WengChienCheng@users.noreply.github.com>
2020-08-14 17:16:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
-var input = document.getElementById('text');
-var button = document.getElementById('send');
-var list = document.getElementById('list');
-
-button.addEventListener("click",addData);
-//button被點後執行addData
-var todolist=[];
-
-function addData() {
-  todolist.push(input.value);
-  tool();
+function divShow(){
+  document.getElementById("btnshow").style.display="block";
+  document.getElementById("btnhref").innerHTML ="Close";
+  document.getElementById("btnhref").href ="javascript:divhidden()";
 }
-var index;
-function delData(index){
-  var temp=[];
-  for (var i=0; i<todolist.length;i++){
-    if (i!=index) temp.push(todolist[i]);
-  }
-  todolist=temp;
-  tool();
-}
-
-
-
-// 用function把重複的code包起來以利重複使用 呼叫即可
-function tool() {
-  var plus = "";
-  for (var i = 0; i < todolist.length; i++) {
-    plus = plus + "<div><button onclick='delData(" + i + ")'>刪除</button>" + todolist[i] + "</div>";
-  }
-  list.innerHTML = plus;
+function divhidden(){
+  document.getElementById("btnshow").style.display="none";
+  document.getElementById("btnhref").innerHTML ="Click";
+  document.getElementById("btnhref").href ="javascript:divShow()"; 
 }
